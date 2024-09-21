a2334e58c5a526480a770f53eb85131ee31eb7f9
手瓜一十雪 <nanaeonn@outlook.com>
2024-09-21 18:22:24 +0800

    boot vnc

--- start.sh
+++ start.sh
@@ -20,6 +20,7 @@ dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address &
 Xvfb :1 -screen 0 1080x760x16 &
 fluxbox &
 x11vnc -display :1 -noxrecord -noxfixes -noxdamage -forever -rfbauth ~/.vnc/passwd &
+nohup /opt/noVNC/utils/novnc_proxy --vnc localhost:5900 --listen 6081 --file-only &
 x11vnc -storepasswd $VNC_PASSWD ~/.vnc/passwd &
 export DISPLAY=:1
 # --disable-gpu 不加入
