0a822506e4d05be1e9caa08cfe9779dd2314abfa
手瓜一十雪 <nanaeonn@outlook.com>
2024-12-14 11:56:57 +0800

    fix

--- LoadLiteLoader.js
+++ LoadLiteLoader.js
@@ -1,14 +1,1 @@
-// From LiteLoader
-const fs = require("fs");
-const path = require("path");
-const package_path = path.join(process.resourcesPath, "app/package.json");
-const package = require(package_path);
-package.main = "./application/app_launcher/index.js";
-fs.writeFileSync(package_path, JSON.stringify(package, null, 4), "utf-8");
-console.log("LiteLoader loaded...");
 require('/opt/QQ/resources/app/LiteLoader/');
-require('./application/app_launcher/index.js');
-setTimeout(() => {
-    package.main = "./LoadLiteLoader.js";
-    fs.writeFileSync(package_path, JSON.stringify(package, null, 4), "utf-8");
-}, 0);
