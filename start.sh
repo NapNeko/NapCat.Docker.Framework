04f755ed9bbde29e1165ae87e8bffbd4a19bfb08
WWWw <109729945+initialencounter@users.noreply.github.com>
2025-02-09 23:47:11 +0800

    fix: windowManager

--- start.sh
+++ start.sh
@@ -20,13 +20,13 @@ sed -i 's/"main": ".\/application\/app_launcher\/index.js"/"main": ".\/LoadLiteL
 rm -rf /run/dbus/pid &
 rm /tmp/.X1-lock &
 mkdir -p /var/run/dbus &
+export DISPLAY=:1
 dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address &
 Xvfb :1 -screen 0 1920x1080x16 &
 fluxbox &
 x11vnc -display :1 -noxrecord -noxfixes -noxdamage -forever -rfbauth ~/.vnc/passwd &
 nohup /opt/noVNC/utils/novnc_proxy --vnc localhost:5900 --listen 6081 --file-only &
 x11vnc -storepasswd $VNC_PASSWD ~/.vnc/passwd &
-export DISPLAY=:1
 
 : ${NAPCAT_GID:=1000}
 : ${NAPCAT_UID:=1000}
